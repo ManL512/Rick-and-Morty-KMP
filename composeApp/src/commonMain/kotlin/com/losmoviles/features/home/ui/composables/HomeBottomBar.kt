c046ea4c90307bcb348ce3676a1b754a2ec087aa
ManL512 <miguelnavas933@gmail.com>
2025-11-06 10:50:21 -0500

    UI modification

--- composeApp/src/commonMain/kotlin/com/losmoviles/features/home/ui/composables/HomeBottomBar.kt
+++ composeApp/src/commonMain/kotlin/com/losmoviles/features/home/ui/composables/HomeBottomBar.kt
@@ -3,6 +3,7 @@ package com.losmoviles.features.home.ui.composables
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.GridView
+import androidx.compose.material.icons.outlined.PersonSearch
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 
@@ -13,7 +14,13 @@ fun HomeBottomBar(selected: HomeTab, onSelect: (HomeTab) -> Unit) {
     NavigationBar {
         NavigationBarItem(selected == HomeTab.HOME, { onSelect(HomeTab.HOME) }, { Icon(Icons.Filled.Home, null) }, label = { Text("Home") }, alwaysShowLabel = false)
         NavigationBarItem(selected == HomeTab.FAVORITES, { onSelect(HomeTab.FAVORITES) }, { Icon(Icons.Filled.Favorite, null) }, label = { Text("Favorites") }, alwaysShowLabel = false)
-        NavigationBarItem(selected == HomeTab.CATALOG, { onSelect(HomeTab.CATALOG) }, { Icon(Icons.Outlined.GridView, null) }, label = { Text("Catalog") }, alwaysShowLabel = false)
+        NavigationBarItem(
+            selected == HomeTab.CATALOG,
+            { onSelect(HomeTab.CATALOG) },
+            { Icon(Icons.Outlined.PersonSearch, null) },
+            label = { Text("Catalog") },
+            alwaysShowLabel = false
+        )
         NavigationBarItem(selected == HomeTab.PROFILE, { onSelect(HomeTab.PROFILE) }, { Icon(Icons.Filled.Person, null) }, label = { Text("Profile") }, alwaysShowLabel = false)
         NavigationBarItem(selected == HomeTab.SETTINGS, { onSelect(HomeTab.SETTINGS) }, { Icon(Icons.Filled.Settings, null) }, label = { Text("Settings") }, alwaysShowLabel = false)
     }
